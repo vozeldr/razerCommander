2a7009705625db076277b9b85a1bb49a7f713076
Dewey Vozel <dewey@vozel.net>
2017-08-05 15:39:36 -0400

    Fixed an error message to not say 'service' and added exception handling to skip over unsupported devices.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ try:
     import device
     print('Device logic loaded, deamon is alive')
 except:
-    print('ERROR: the daemon is not responding!\nTry running `killall razer-service && razer-service` or rebooting. If this doesn\'t work, please fill an issue!')
+    print('ERROR: the daemon is not responding!\nTry running `killall razer-daemon && razer-daemon` or rebooting. If this doesn\'t work, please fill an issue!')
     exit(1)
 import custom_keyboard as CustomKb
 import custom_profiles
@@ -41,8 +41,12 @@ def initDevices():
     global devicesList
     devicesList = []
     for dev in device.devlist:
-        newdev = device.Device(dev)
-        devicesList.append(newdev)
+        try:
+            newdev = device.Device(dev)
+            devicesList.append(newdev)
+        except:
+            print('Skipping device: ' + dev.name)
+            pass
 
 def fillDevicesList():
     if len(devicesList) > 0:
